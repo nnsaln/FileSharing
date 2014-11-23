87c387b0fe46e0613b4b798b1335005507b0ed93
I Gede Putu Surya <surya.igede@gmail.com>
2014-11-23 18:43:59 +0700

    Update FileServer.java
    sudah bisa upload dan download dari/ke client

--- BerbagiBerkas2/Server/FileServer.java
+++ BerbagiBerkas2/Server/FileServer.java
@@ -1,13 +1,15 @@
-package filesharing;
+package serverberbagiberkas2;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class FileServer {
 
     private static ServerSocket serverSocket;
     private static Socket clientSocket = null;
+    private static ArrayList<CLIENTConnection> alThread = new ArrayList();
 
     public static void main(String[] args) throws IOException {
 
@@ -23,14 +25,25 @@ public class FileServer {
             try {
                 clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection : " + clientSocket);
-
-                Thread t = new Thread(new CLIENTConnection(clientSocket));
-
+                
+                Thread t;
+                CLIENTConnection curr = new CLIENTConnection(clientSocket, alThread);
+                t = new Thread(curr);
                 t.start();
+                synchronized(alThread)
+                {
+                    alThread.add(curr);
+                    for (int i = 0; i < alThread.size(); i++)
+                    {
+                        alThread.get(i).setAL(alThread);
+                    }
+                }
 
             } catch (Exception e) {
                 System.err.println("Error in connection attempt.");
             }
         }
+        //serverSocket.close();
     }
+
 }
