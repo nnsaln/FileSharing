4f6938269fd5d795b83af95125855499ad35b334
I Gede Putu Surya <surya.igede@gmail.com>
2014-11-24 12:01:42 +0700

    Update FileServer.java
    sudah bisa list dan close

--- BerbagiBerkas2/Server/FileServer.java
+++ BerbagiBerkas2/Server/FileServer.java
@@ -1,21 +1,26 @@
 package serverberbagiberkas2;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class FileServer {
-
+    private static int servport;
     private static ServerSocket serverSocket;
     private static Socket clientSocket = null;
     private static ArrayList<CLIENTConnection> alThread = new ArrayList();
 
     public static void main(String[] args) throws IOException {
-
+        System.out.print("Masukkan port yg ingin digunakan: ");
+        BufferedReader port = new BufferedReader(new InputStreamReader(System.in));
+        servport = Integer.parseInt(port.readLine());
         try {
-            serverSocket = new ServerSocket(4444);
-            System.out.println("Server started.");
+            serverSocket = new ServerSocket(servport);
+            System.out.println("Server berjalan pada port " + servport + ".");
+            System.out.println("Menunggu permintaan...");
         } catch (Exception e) {
             System.err.println("Port already in use.");
             System.exit(1);
