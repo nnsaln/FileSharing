e199c34e6436b0e903e1245f661a3c9feec03d31
I Gede Putu Surya <surya.igede@gmail.com>
2014-11-24 11:25:30 +0700

    Update FileClient.java
    tambahan fungsi baru: list user

--- BerbagiBerkas2/Client/FileClient.java
+++ BerbagiBerkas2/Client/FileClient.java
@@ -8,6 +8,7 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -25,11 +26,20 @@ public class FileClient {
     private static String fileName;
     private static BufferedReader stdin;
     private static PrintStream os;
+    private static String servaddr;
+    private static int servport;
     //private ArrayList<namaUser>;
     
     public static void main(String[] args) throws IOException {
+        BufferedReader servdetail = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Masukkan alamat server yang dituju: ");
+        servaddr = servdetail.readLine();
+        System.out.print("Masukkan port server yang dituju: ");
+        servport = Integer.parseInt(servdetail.readLine());
+        
         try {
-            sock = new Socket("localhost", 4444);
+            sock = new Socket(servaddr, servport);
+            System.out.println("Terhubung dengan server " + servaddr + " pada port " + servport);
             stdin = new BufferedReader(new InputStreamReader(System.in));
             
         } catch (Exception e) {
@@ -70,9 +80,31 @@ public class FileClient {
                             Logger.getLogger(CLIENTConnection.class.getName()).log(Level.SEVERE, null, ex);
                     }
                     break;
-                // case 3:
-                  //  os.println("3");
-                    //listUser();
+                case 3:
+                    os.println("3");
+                    String user;
+                    System.out.print("List User:\n");
+                    while((user = clientData.readUTF()) != null)
+                    {
+                        System.out.println(user);
+                    }
+                    break;
+                case 4:
+                    os.println("4");
+                    //sur ini pake list folder dari serverku, kalo mau nyoba pake folder servermu sur
+                    File folder = new File("C:/Users/An Nisa/Documents/NetBeansProjects/FileSharing/");
+                    File[] listOfFiles = folder.listFiles();
+                    System.out.println("List of Files:");
+                    for (int i = 0; i < listOfFiles.length; i++) {
+                        if (listOfFiles[i].isFile()) 
+                        { 
+                            System.out.println(listOfFiles[i].getName());
+                        } else if (listOfFiles[i].isDirectory()) {
+                        //System.out.println("Directory " + listOfFiles[i].getName());
+                        continue;
+                        }
+                    }
+                    break;
                 default:
                     os.println("4");
                     System.out.println("Incorrect command received.");
@@ -81,7 +113,7 @@ public class FileClient {
             }
         in.close();
         } catch (Exception e) {
-            System.err.println("not valid input");
+            System.err.println("not valid input" + e.getMessage());
         }
 
 
@@ -91,7 +123,8 @@ public class FileClient {
     public static String selectAction() throws IOException {
         System.out.println("1. Upload file.");
         System.out.println("2. Download file.");
-        System.out.println("3. List Active User.");
+        System.out.println("3. List Active User. (refresh)");
+        System.out.println("4. List File.");
         System.out.print("\nMake selection: ");
 
         return stdin.readLine();
